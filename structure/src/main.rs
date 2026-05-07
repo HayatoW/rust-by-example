705af3dcdd26c1dd0719e36342d2888c3a7f8162
Hayato Watanabe <solideye@me.com>
2026-05-07 14:03:20 +0000

    自分で書いてみた

---
+++ structure/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+impl Rectangle {
+    fn rect_area(&self) -> f32 {
+        let Point { x: x1, y: y1 } = self.top_left;
+        let Point { x: x2, y: y2 } = self.bottom_right;
+
+        (x1 - x2).abs() * (y1 - y2).abs()
+    }
+}
+
+#[test]
+fn test_rect_area() {
+    let rectangle = Rectangle {
+        top_left: Point { x: 0f32, y: 2f32 },
+        bottom_right: Point { x: 2f32, y: 0f32 },
+    };
+
+    assert_eq!(rectangle.rect_area(), 4f32);
+}
